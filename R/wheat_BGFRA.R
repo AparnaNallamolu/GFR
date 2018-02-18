2a7543291be8e2c9bf5b3ce41aa80050a96eaf91
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-02-17 20:12:51 -0600

    add wheat_BGFRA documentation

---
+++ R/wheat_BGFRA.R
@@ -0,0 +1,10 @@
+#' @name wheat_BGFRA
+#' @title Wheat Data
+#' @description The package includes a data set based on a portion of the data used in the study of
+#'
+#' @docType data
+#' @usage data(wheat_BGFRA)
+#' @format a \code{TidyData} format, 3000 row per 4 columns.
+#' @author Montesinos-Lopez, O. A.
+#'
+NULL
