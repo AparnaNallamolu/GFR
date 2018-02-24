175c052245f38114059cd005679adb9af9565c05
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-02-23 23:14:02 -0600

    update name

--- R/wheat_BGFRA.R
+++ R/wheat_BGFRA.R
@@ -1,9 +1,9 @@
-#' @name wheat_BGFRA
+#' @name wheat_BFR
 #' @title Wheat Data
 #' @description The package includes a data set based on a portion of the data used in the study of
 #'
 #' @docType data
-#' @usage data(wheat_BGFRA)
+#' @usage data(wheat_BFR)
 #' @format a \code{TidyData} format, 3000 row per 4 columns.
 #' @author Montesinos-Lopez, O. A.
 #'
