18f7f5fff09fee01a7d7964d9de9026a62fded55
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-02-28 10:25:53 -0600

    Fixed datasets
    - Maize dataset was added
    - Wheat dataset was fixed

--- R/Maize_BFR.R
+++ R/Maize_BFR.R
@@ -1,9 +1,9 @@
-#' @name wheat_BFR
-#' @title Wheat Data
+#' @name Maize_BFR
+#' @title Maize Data
 #' @description The package includes a data set based on a portion of the data used in the study of
 #'
 #' @docType data
-#' @usage data(wheat_BFR)
+#' @usage data('Maize_BFR')
 #' @format a \code{TidyData} format, 3000 row per 4 columns.
 #' @author Montesinos-Lopez, O. A.
 #'
---
+++ R/Wheat_BFR.R
@@ -0,0 +1,10 @@
+#' @name Wheat_BFR
+#' @title Wheat Data
+#' @description The package includes a data set based on a portion of the data used in the study of
+#'
+#' @docType data
+#' @usage data('Wheat_BFR')
+#' @format a \code{TidyData} format, 3000 row per 4 columns.
+#' @author Montesinos-Lopez, O. A.
+#'
+NULL
