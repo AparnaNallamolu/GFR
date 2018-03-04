6ee08f40647a4852579071211b7843337485e2bf
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-03-04 14:12:50 -0600

    new dataset added

---
+++ R/WheatI_GFR.R
@@ -0,0 +1,10 @@
+#' @name WheatI_GFR
+#' @title Wheat Data
+#' @description The package includes a data set based on a portion of the data used in the study of
+#'
+#' @docType data
+#' @usage data('WheatI_GFR')
+#' @format \code{WheatI_GFR) is \code{data.frame} in \code{TidyData} format, 976 row per 3 columns. Also is loaded \code{WheatI_Wavelengths} and \code{WheatI_Bands} which includes the names of the bandas and the bands respectively.
+#' @author Montesinos-Lopez, O. A.
+#'
+NULL
