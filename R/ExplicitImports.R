64546513409c921c7d9a3f34f5ed87bed2ef4eac
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-07-19 16:19:33 -0500

    Explicit functions imports
    Now the package includes getTidyForm, getMatrixForm and Cholesky function from IBCF.MTME package and BMTME package

---
+++ R/ExplicitImports.R
@@ -0,0 +1,30 @@
+#' Get the tidy format from a matrix format
+#'
+#' @name getTidyForm
+#' @rdname getTidyForm
+#' @keywords internal
+#' @export
+#' @importFrom IBCF.MTME getTidyForm
+#' @usage getTidyForm(data, onlyTrait = T)
+NULL
+
+#' Get the matrix format from a tidy format
+#'
+#' @name getMatrixForm
+#' @rdname getMatrixForm
+#' @keywords internal
+#' @export
+#' @importFrom IBCF.MTME getMatrixForm
+#' @usage getMatrixForm(data, onlyTrait = T)
+NULL
+
+
+#' Cholesky
+#'
+#' @name cholesky
+#' @rdname cholesky
+#' @keywords internal
+#' @export
+#' @importFrom BMTME cholesky
+#' @usage cholesky(a)
+NULL
