b63d70bd5a226ed0db6e2b02e259043f83d8f8a3
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-07-20 00:27:58 -0500

    Imports detectCores() function

--- R/ExplicitImports.R
+++ R/ExplicitImports.R
@@ -28,3 +28,13 @@ NULL
 #' @importFrom BMTME cholesky
 #' @usage cholesky(a)
 NULL
+
+#' Detect number of cores
+#'
+#' @name detectCores
+#' @rdname detectCores
+#' @keywords internal
+#' @export
+#' @importFrom parallel detectCores
+#' @usage detectCores()
+NULL
