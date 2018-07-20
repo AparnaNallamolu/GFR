5d38d430a3b4bb6e694038ce21197461849fa139
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-07-20 00:29:06 -0500

    include show parameter

--- R/Information.R
+++ R/Information.R
@@ -2,8 +2,8 @@ Error <- function(message, ...) {
   stop('[x] ', message, ..., '\n', call. = FALSE)
 }
 
-Message <- function(message, ...) {
-  message('[i] ', message, ...)
+Message <- function(message, show = TRUE, ...) {
+  if(show){ message('[i] ', message, ...)}
 }
 
 Warning <- function(message, ...) {
