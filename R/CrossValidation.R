833e10da23708fa0dff7c31b7af6a5c95104d640
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-07-25 14:51:54 -0500

    Import new functions
    Import CV. stratifiedByFrac() function
    Import CV. stratified() function

--- R/CrossValidation.R
+++ R/CrossValidation.R
@@ -18,3 +18,33 @@ NULL
 #' @importFrom BMTME CV.KFold
 #' @usage CV.KFold(data, DataSetID = 'Line', K = 5, set_seed =  123)
 NULL
+
+#' Cross-validation with K Folds
+#'
+#' @name CV.KFold
+#' @rdname CV.KFold
+#' @keywords internal
+#' @export
+#' @importFrom BMTME CV.KFold
+#' @usage CV.KFold(data, DataSetID = 'Line', K = 5, set_seed =  123)
+NULL
+
+#' Cross-validation with stratified samples
+#'
+#' @name CV.StratifiedByFrac
+#' @rdname CV.StratifiedByFrac
+#' @keywords internal
+#' @export
+#' @importFrom BMTME CV.StratifiedByFrac
+#' @usage CV.StratifiedByFrac(DataSet, NSamples = 10, fracTesting = 0.1, replace = FALSE, set_seed = NULL)
+NULL
+
+#' Cross-Validation with stratified samples
+#'
+#' @name CV.Stratified
+#' @rdname CV.Stratified
+#' @keywords internal
+#' @export
+#' @importFrom BMTME CV.Stratified
+#' @usage CV.Stratified(DataSet, NSamples = 10, nTesting = 10, replace = FALSE, set_seed = NULL)
+NULL
