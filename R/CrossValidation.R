676e4a265fac472b2029fff903db2e5a707f84ad
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-08-21 15:20:35 -0500

    remove a function

--- R/CrossValidation.R
+++ R/CrossValidation.R
@@ -19,16 +19,6 @@ NULL
 #' @usage CV.KFold(data, DataSetID = 'Line', K = 5, set_seed =  123)
 NULL
 
-#' Cross-validation with K Folds
-#'
-#' @name CV.KFold
-#' @rdname CV.KFold
-#' @keywords internal
-#' @export
-#' @importFrom BMTME CV.KFold
-#' @usage CV.KFold(data, DataSetID = 'Line', K = 5, set_seed =  123)
-NULL
-
 #' Cross-validation with stratified samples
 #'
 #' @name CV.StratifiedByFrac
